265e34320abdb8e3a090810462aa97e7c1dc6c6f
Alexander Böckle <alexander.boeckle@Alexanders-MacBook-Pro.local>
2018-09-12 15:58:13 +0200

    Reading and printing map

--- src/Controls.java
+++ src/Controls.java
@@ -1,24 +1,18 @@
 import java.io.*;
 
 public class Controls {
-    private Map map = new Map();
+    private Map map;
     private Interface theInterface = new Interface();
 
-    private String mapString;
-
     public void start() throws FileNotFoundException, IOException{
-        readMap();
+        map = new Map(readMap());
 
-
-
-        System.out.println(mapString);
-
-        //theInterface.printMap(map.getMap(), map.getObjects());
+        theInterface.printMap(map.getMap(), map.getObjects());
     }
 
-    private void readMap() throws FileNotFoundException, IOException {
+    private String readMap() throws FileNotFoundException, IOException {
         File mapFile = new File("map.txt");
         BufferedReader reader = new BufferedReader(new FileReader(mapFile));
-        mapString = reader.readLine();
+        return reader.readLine();
     }
 }
--- src/Map.java
+++ src/Map.java
@@ -2,14 +2,14 @@ public class Map {
     private int[][] map = new int[20][40];
     private final String[] objects = {" ", "#", "S", "R", "G", "+"};
 
-    public Map(){
-        initMap();
+    public Map(String mapString){
+        initMap(mapString);
     }
 
-    public void initMap() {
+    public void initMap(String mapString) {
         for (int y = 0; y < 40; y++) {
             for (int x = 0; x < 20; x++) {
-                map[x][y] = 2;
+                map[x][y] = Character.getNumericValue(mapString.charAt(x + y*20));
             }
         }
     }
